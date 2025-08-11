9bff943c11143dcc6f05ba9ecf40d1e284402e4b
covstar <okechukwuvictor093@gmail.com>
2025-08-11 07:34:18 -0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div className="p-4 h-screen flex items-center text-center justify-center bg-green-700">
+			<h1 className="text-5xl font-bold text-orange-400">Hello, Africa Markett!</h1>
+		</div>
+	);
+}
